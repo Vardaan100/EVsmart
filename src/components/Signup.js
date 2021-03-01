9642901bcfccd262b280e4c66a9190205d24b9da
vibhumishra74 <mail.ashishmishra74@gmail.com>
2021-03-01 16:59:06 +0530

    some basic css added

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,64 +1,64 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./signup.css";
 
 export default class SignUp extends Component {
-  
   render() {
     return (
-      <form>
-      <h3>Sign Up</h3>
+      <form className="signup__container">
+        <h3>Sign Up</h3>
 
-      <div className="form-group">
-        <label>First name</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="First name"
-        />
-      </div>
+        <div className="form-group">
+          <label>First name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="First name"
+          />
+        </div>
 
-      <div className="form-group">
-        <label>Last name</label>
-        <input type="text" className="form-control" placeholder="Last name" />
-      </div>
+        <div className="form-group">
+          <label>Last name</label>
+          <input type="text" className="form-control" placeholder="Last name" />
+        </div>
 
-      <div className="form-group">
-        <label>Phone Number</label>
-        <input
-          type="number"
-          className="form-control"
-          placeholder="Enter phone number"
-        />
-      </div>
+        <div className="form-group">
+          <label>Phone Number</label>
+          <input
+            type="number"
+            className="form-control"
+            placeholder="Enter phone number"
+          />
+        </div>
 
-      <div className="form-group">
-        <label>Email address</label>
-        <input
-          type="email"
-          className="form-control"
-          placeholder="Enter email"
-        />
-      </div>
+        <div className="form-group">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
 
-      <div className="form-group">
-        <label>Password</label>
-        <input
-          type="password"
-          className="form-control"
-          placeholder="Enter password"
-        />
-      </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
 
-      <button type="submit" className="btn btn-primary btn-block">
-        Sign Up
-      </button>
-      <p className="forgot-password text-right">
-        Already registered{" "}
-        <Link className="nav-link" to={"/sign-in"}>
-          sign in?
-        </Link>
-      </p>
-    </form>
+        <button type="submit" className="btn btn-primary btn-block">
+          Sign Up
+        </button>
+        <p className="forgot-password text-right">
+          Already registered{" "}
+          <Link className="nav-link " to={"/sign-in"}>
+            <span className="signup__login">sign in?</span>
+          </Link>
+        </p>
+      </form>
     );
   }
 }
--- src/components/home components/Home.js
+++ src/components/home components/Home.js
@@ -16,17 +16,20 @@ function Home() {
           <br></br>
           <br></br>
           <p>
-          EV Smart aims to provide service for people who needs to charge their vehicle
-          away from home. At EV Smart service is given by people to people.
-          Sign up with us and locate charging station near you.
-          You can also add your own station as well, just head to the station option fill out the
-          form and your station will be added to stations list
-          <br></br>
-          <br></br>
-          Be the change for better tomorrow
+            EV Smart aims to provide service for people who needs to charge
+            their vehicle away from home. At EV Smart service is given by people
+            to people. Sign up with us and locate charging station near you. You
+            can also add your own station as well, just head to the station
+            option fill out the form and your station will be added to stations
+            list
+            <br></br>
+            <br></br>
+            Be the change for better tomorrow
           </p>
-          <Link to="/sign-in">
-          <Button vairent="contained">Find Charging point</Button>
+          <Link style={{ textDecoration: "none" }} to="/sign-in">
+            <Button className="home__chargingpoint" vairent="contained">
+              Find Charging point
+            </Button>
           </Link>
         </div>
         <div className="home__image">
