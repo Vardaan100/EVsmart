3a2782f8e95f12d45616090daa5a33cd25247a5a
Suraj Pawar <spawar1530@gmail.com>
2021-03-07 14:06:50 +0530

    admin

--- src/components/adminDash.js
+++ src/components/adminDash.js
@@ -1,65 +1,59 @@
-import React, { Component } from "react";
-import { getAllDash } from "../fetchingData/api_calls";
-import "./profile.css";
-import { JsonToTable } from "react-json-to-table";
+// import React, { Component } from "react";
+// import { getAllDash } from "../fetchingData/api_calls";
+// import "./profile.css";
+// import { JsonToTable } from "react-json-to-table";
 
-class AdminDash extends Component {
-  constructor(props) {
-    super(props);
+// class AdminDash extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      users: [],
-    };
-  }
+//     this.state = {
+//       users: [],
+//     };
+//   }
 
-  componentDidMount() {
-    const token = localStorage
-      .getItem("jwt", JSON.stringify())
-      .replaceAll('"', "");
-    getAllDash(token).then((data) => {
-      data.map((user_id, idx) => {
-        this.setState({
-          users: data,
-        });
-      });
-    });
-  }
+//   componentDidMount() {
+//     const token = localStorage
+//       .getItem("jwt", JSON.stringify())
+//       .replaceAll('"', "");
+//     getAllDash(token).then((data) => {
+//       data.map((user_id, idx) => {
+//         this.setState({
+//           users: data,
+//         });
+//       });
+//     });
+//   }
 
-  render() {
-    return (
-      <div className="adminDash">
-        <JsonToTable json={this.state.users} />
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="adminDash">
+//         <JsonToTable json={this.state.users} />
+//       </div>
+//     );
+//   }
+// }
 
-export default AdminDash;
+// export default AdminDash;
 
 
-// import * as React from "react";
-// import { Admin, Resource, ListGuesser } from 'react-admin';
-// import jsonServerProvider from 'ra-data-json-server';
-// import { createHashHistory } from "history";
-// import adminStore from "./CreateadminStore";
-// import { Provider } from "react-redux";
+import * as React from "react";
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import { createHashHistory } from "history";
+import { Provider } from "react-redux";
 
-// const dataProvider = jsonServerProvider
-//  (
-// 'https://jsonplaceholder.typicode.com'
-//  );
+const dataProvider = jsonServerProvider
+ (
+'https://jsonplaceholder.typicode.com'
+ );
 
-//  const history = createHashHistory();
+const AdminDash = () => 
 
-// const adminDash = () => 
-// <Provider
-// store={adminStore({
-//     dataProvider,
-//     history,
-// })}
-// >
-// <Admin dataProvider={dataProvider} history={history}/>;
+<Admin dataProvider={dataProvider} >
 
-// </Provider>
+<Resource name="users" list={ListGuesser} />
 
-// export default adminDash
+</Admin>;
+
+export default AdminDash
